820ba72b5c99c3701c5315f6e2fe9cece0f6a0cb
Ankit Meena <95204345+Ankitjarwall@users.noreply.github.com>
2023-12-11 11:43:58 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [userInput, setUserInput] = useState('');
+  const [youtubeUrl, setYoutubeUrl] = useState('');
+
+  const handleInputChange = (e) => {
+    setUserInput(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newYoutubeUrl = `https://www.youtube.com/embed/${userInput}`;
+    setYoutubeUrl(newYoutubeUrl);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>YouTube Player App</h1>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Enter YouTube Video ID:
+            <input type="text" value={userInput} onChange={handleInputChange} />
+          </label>
+          <button type="submit">Submit</button>
+        </form>
+        {youtubeUrl && (
+          <iframe
+            title="YouTube Player"
+            width="560"
+            height="315"
+            src={youtubeUrl}
+            frameborder="0"
+            allowfullscreen
+          ></iframe>
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
